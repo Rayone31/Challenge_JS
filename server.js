682778c939f4aaf42a269ebacd743ee668de24bf
hdylan <hebrard.dylan@ynov.com>
2024-05-21 14:15:52 +0200

    Add: server Mise a jour

---
+++ server.js
@@ -0,0 +1,34 @@
+// Importer le module Express
+const express = require('express');
+const path = require('path');
+// Créer une nouvelle application Express
+const app = express();
+// Définir le dossier public comme dossier statique
+app.use(express.static('public'));
+// Définir une route GET pour la racine ("/")
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname,'public', 'html', 'index.html'));
+});
+app.get('/snake', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'html', 'snake.html'));
+});
+app.get('/tri', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'html', 'tri.html'));
+});
+app.get('/morpion', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'html', 'morpion.html'));
+});
+
+app.get('/cdf', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'html', 'cdf.html'));
+});
+
+app.get('/cdf/data', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'data', 'cdf.json'));
+});
+
+
+// Démarrer le serveur sur le port 3000
+app.listen(3000, () => {
+  console.log('Le serveur est en écoute sur le port 3000 au port http://localhost:3000/');
+});
