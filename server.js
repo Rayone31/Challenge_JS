c975a412a2b4f1791167cad40637e8fcf66b5585
hdylan <hebrard.dylan@ynov.com>
2024-05-31 17:09:05 +0200

    Add snake debug V1

--- server.js
+++ server.js
@@ -1,34 +1,22 @@
 // Importer le module Express
 const express = require('express');
 const path = require('path');
-
 // Créer une nouvelle application Express
 const app = express();
-
 // Définir le dossier public comme dossier statique
 app.use(express.static('public'));
-
-// Définir le dossier public comme dossier statique
-app.use(express.static('public'));
-
 // Définir une route GET pour la racine ("/")
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname,'public', 'html', 'index.html'));
 });
-
 app.get('/snake', function (req, res) {
   res.sendFile(path.join(__dirname, 'public', 'html', 'snake.html'));
 });
 app.get('/tri', function (req, res) {
   res.sendFile(path.join(__dirname, 'public', 'html', 'tri.html'));
 });
-
 app.get('/morpion', function (req, res) {
   res.sendFile(path.join(__dirname, 'public', 'html', 'morpion.html'));
-});
-
-app.get('/VMT', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'html', 'VMT.html'));
 });
 
 app.get('/cdf', function (req, res) {
