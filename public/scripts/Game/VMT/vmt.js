37a53ee0c40fd5d06abba7f19eb7ddf85d198909
hdylan <hebrard.dylan@ynov.com>
2024-06-11 13:41:53 +0200

    Add index Final V2

--- public/scripts/Game/VMT/vmt.js
+++ public/scripts/Game/VMT/vmt.js
@@ -1,8 +1,11 @@
 let selectedSquares = [];
 let userSelections = [];
-let difficulty = 3; // Nombre de cases à sélectionner
-let gamesWon = 0; // Nombre de parties réussies
+let difficulty = 3; 
+let gamesWon = 0; 
 let size = 3; // 3x3 damier
+let isGamePlayable = true; 
+let lives = 3;
+
 
 // Stocker les valeurs de départ
 let initialDifficulty = difficulty;
@@ -17,7 +20,7 @@ for (let i = 0; i < size * size; i++) {
     square.style.width = '100px';
     square.style.height = '100px';
     square.style.border = '1px solid black';
-    square.style.backgroundColor = 'blue';
+    square.style.backgroundColor = 'yellow';
     square.style.borderRadius = '15px';
     damier.appendChild(square);
 }
@@ -34,7 +37,7 @@ while (selectedSquares.length < difficulty) {
 // Après 2 secondes, les cases deviennent bleues
 setTimeout(() => {
     for (let i = 0; i < size * size; i++) {
-        document.getElementById('square' + i).style.backgroundColor = 'blue';
+        document.getElementById('square' + i).style.backgroundColor = 'yellow';
     }
 }, 2000);
 
@@ -56,14 +59,14 @@ function checkSelections() {
         userSelections.sort();
         selectedSquares.sort();
         if (JSON.stringify(userSelections) === JSON.stringify(selectedSquares)) {
-            //alert('Bravo! Vous avez réussi le test de mémoire visuelle.');
             gamesWon++;
             difficulty++;
+            updateLevelDisplay(); 
             if (gamesWon % 3 === 0) {
                 size++;
             }
             setTimeout(() => {
-            resetTest();
+                resetTest();
             }, 1000);
         } else {
             alert('Désolé, vous avez échoué au test de mémoire visuelle. Cliquez sur le bouton "Réinitialiser" pour recommencer.');
@@ -75,23 +78,35 @@ function resetTest() {
     // Réinitialiser les tableaux
     selectedSquares = [];
     userSelections = [];
+    errorsCount = 0;
+    isGamePlayable = true;
+    lives = 3;
+    updateLivesDisplay();
 
     // Supprimer l'ancien damier
     damier.innerHTML = '';
 
-    // Mettre à jour la grille
-    damier.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    damier.style.width = `${size * 120}px`; 
-    damier.style.height = `${size * 120}px`;
+    // Définir une marge constante pour chaque damier
+    const marginPerSquare = 5; 
 
-    // Créer un nouveau damier de la nouvelle taille
+    // Calculer la nouvelle taille de chaque damier pour maintenir la taille globale constante, en tenant compte de la marge
+    let totalAvailableSize = initialSize * (100 + 2 * marginPerSquare) - (size * 2 * marginPerSquare);
+    let squareSize = totalAvailableSize / size; 
+
+    // Mettre à jour la grille pour la nouvelle taille, en tenant compte de la marge
+    damier.style.gridTemplateColumns = `repeat(${size}, ${squareSize}px)`;
+    damier.style.width = `${initialSize * (100 + 2 * marginPerSquare)}px`; 
+    damier.style.height = `${initialSize * (100 + 2 * marginPerSquare)}px`; 
+
+    // Créer un nouveau damier de la nouvelle taille, en incluant la marge
     for (let i = 0; i < size * size; i++) {
         let square = document.createElement('div');
         square.id = 'square' + i;
-        square.style.width = '100px';
-        square.style.height = '100px';
+        square.style.width = `${squareSize - 2 * marginPerSquare}px`; 
+        square.style.height = `${squareSize - 2 * marginPerSquare}px`; 
+        square.style.margin = `${marginPerSquare}px`;
         square.style.border = '1px solid black';
-        square.style.backgroundColor = 'blue';
+        square.style.backgroundColor = 'yellow';
         square.style.borderRadius = '15px';
         damier.appendChild(square);
     }
@@ -108,14 +123,37 @@ function resetTest() {
     // Après 2 secondes, les cases deviennent bleues et les écouteurs d'événements sont ajoutés
     setTimeout(() => {
         for (let i = 0; i < size * size; i++) {
-            document.getElementById('square' + i).style.backgroundColor = 'blue';
+            document.getElementById('square' + i).style.backgroundColor = 'yellow';
             document.getElementById('square' + i).addEventListener('click', userClick);
         }
     }, 2000);
 }
 
 function userClick() {
-    userSelections.push(parseInt(this.id.replace('square', '')));
-    this.style.backgroundColor = 'white';
-    checkSelections();
+    if (!isGamePlayable) {
+        return; 
+    }
+    let squareId = parseInt(this.id.replace('square', ''));
+    if (!selectedSquares.includes(squareId)) {
+        this.style.backgroundColor = 'red';
+        lives--; // Étape 2: Décrémenter les vies
+        updateLivesDisplay(); // Mettre à jour l'affichage des vies
+        if (lives === 0) {
+            // Étape 3: Vérifier si le jeu doit se terminer
+            alert('Désolé, vous avez perdu toutes vos vies. Cliquez sur le bouton "Réinitialiser" pour recommencer.');
+            isGamePlayable = false;
+        }
+    } else {
+        userSelections.push(squareId);
+        this.style.backgroundColor = 'green';
+        checkSelections();
+    }
+}
+
+function updateLivesDisplay() {
+    document.getElementById('livesDisplay').innerText = `Vies restantes: ${lives}`;
+}
+
+function updateLevelDisplay() {
+    document.getElementById('levelDisplay').innerText = `Niveau : ${gamesWon}`;
 }
--- public/scripts/index/index.js
+++ public/scripts/index/index.js
@@ -1,39 +1,123 @@
 const canvas = document.getElementById('canvas');
-        const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
+
+let selectedIndex = 0;
+const games = ['Snake', 'Morpion', 'Tri à bulle', 'Visual Memory Test', 'Calculateur de frappe'];
+const gameUrls = ['/snake', '/morpion', '/tri', '/VMT', '/cdf'];
+const gameImages = [];
+const imageFilenames = ['snake.jpg', 'morpion.jpg', 'tri.jpg', 'VMT.jpg', 'cdf.jpg'];
+
+imageFilenames.forEach((filename, index) => {
+    const img = new Image();
+    img.onload = function() {
+        gameImages[index] = img;
+        if (index === 0) draw(); 
+    };
+    img.src = `/assets/images/index/${filename}`;
+});
+
+function drawRoundedRect(ctx, x, y, width, height, radius) {
+    ctx.beginPath();
+    ctx.moveTo(x + radius, y);
+    ctx.lineTo(x + width - radius, y);
+    ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+    ctx.lineTo(x + width, y + height - radius);
+    ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+    ctx.lineTo(x + radius, y + height);
+    ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+    ctx.lineTo(x, y + radius);
+    ctx.quadraticCurveTo(x, y, x + radius, y);
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+}
+
+function draw() {
+    ctx.fillStyle = '#2D2D2D'; 
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    let outerRectX = 5;
+    let outerRectY = 5;
+    let outerRectWidth = canvas.width - 370;
+    let outerRectHeight = 300 + 50;
+    let outerRectRadius = 20;
+
+    ctx.fillStyle = '#007FFF'; 
+    drawRoundedRect(ctx, outerRectX, outerRectY, outerRectWidth, outerRectHeight, outerRectRadius);
+
+    let titleRectX = outerRectX + 10;
+    let titleRectY = outerRectY + 10;
+    let titleRectWidth = outerRectWidth - 20;
+    let titleRectHeight = 40;
+    ctx.fillStyle = '#FFA500'; 
+    ctx.fillRect(titleRectX, titleRectY, titleRectWidth, titleRectHeight);
+
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 2;
+    ctx.strokeRect(titleRectX, titleRectY, titleRectWidth, titleRectHeight);
+
+    let rightRectX = canvas.width - 360; // Assurez-vous que cela ne chevauche pas d'autres éléments
+    let rightRectY = 5; // Aligné avec le haut des autres éléments
+    let rightRectWidth = 350; // Largeur du rectangle
+    let rightRectHeight = canvas.height - 10; // Hauteur du rectangle
+
+    // Définir la couleur et dessiner le rectangle
+    ctx.fillStyle = '#4D4D4D'; // Couleur de fond du rectangle
+    ctx.fillRect(rightRectX, rightRectY, rightRectWidth, rightRectHeight);
+    drawRoundedRect(ctx, rightRectX, rightRectY, rightRectWidth, rightRectHeight, 10, '#4D4D4D', 'black');
+
+    let innerRectX = rightRectX + 20; // 20 pixels à l'intérieur du rectangle de droite
+    let innerRectY = rightRectY + 70; // Position Y inchangée
+    let innerRectWidth = rightRectWidth - 40; // Largeur inchangée
+    let innerRectHeight = 175; // Hauteur augmentée de 50 pixels
     
-        let selectedIndex = 0;
-        const games = ['Snake', 'Morpion', 'Tri à bulle','Visual Memory Test', 'Calculateur de frappe'];
-        const gameUrls = ['/snake', '/morpion', '/tri','/VMT', '/cdf'];
-        const markerYPositions = [200, 250, 300, 350];
+    // Définir la couleur et dessiner le rectangle intérieur
+    ctx.fillStyle = '#FFFFFF'; // Couleur de fond du rectangle intérieur
+    ctx.fillRect(innerRectX, innerRectY, innerRectWidth, innerRectHeight);
     
-        function draw() {
-            ctx.fillStyle = 'blue';
-            ctx.fillRect(0, 0, canvas.width, canvas.height);
-    
-            ctx.fillStyle = 'white';
-            ctx.font = '50px Arial';
-            ctx.fillText('Liste des jeux', 50, 100);
-    
-            ctx.font = '30px Arial';
-            games.forEach((game, index) => {
-                ctx.fillText(game, 50, markerYPositions[index]);
-            });
-    
-            ctx.fillStyle = 'yellow';
-            ctx.fillRect(30, markerYPositions[selectedIndex] - 20, 10, 30);
-        }
-    
-        function moveMarker(e) {
-            if (e.key === 'ArrowUp' && selectedIndex > 0) {
-                selectedIndex--;
-            } else if (e.key === 'ArrowDown' && selectedIndex < games.length - 1) {
-                selectedIndex++;
-            } else if (e.key === 'Enter') {
-                window.location.href = gameUrls[selectedIndex];
-            }
-    
-            draw();
-        }
-    
-        draw();
-        window.addEventListener('keydown', moveMarker);
+    // Optionnel : Ajouter un contour au rectangle intérieur
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 2;
+    ctx.strokeRect(innerRectX, innerRectY, innerRectWidth, innerRectHeight);
+
+    if (gameImages[selectedIndex]) {
+        ctx.drawImage(gameImages[selectedIndex], rightRectX + 20, rightRectY + 70, rightRectWidth - 40, 175);
+    }
+
+    ctx.font = '23px "Press Start 2P"'; // Police style arcade
+    ctx.fillStyle = 'white';
+    ctx.fillText("Liste de jeu", titleRectX + 15, titleRectY + 28);
+
+
+    games.forEach((game, index) => {
+        const gameNumber = (index < 9 ? '0' : '') + (index + 1);
+        const text = `${gameNumber}. ${game}`;
+        let x = outerRectX + 20;
+        let y = titleRectY + titleRectHeight + 10 + (index * 50);
+        let width = titleRectWidth - 10;
+        let height = 40;
+        let radius = 10; 
+
+        ctx.fillStyle = selectedIndex === index ? '#00FF00' : '#D3D3D3'; // Vert lime pour la sélection, gris clair sinon
+        drawRoundedRect(ctx, x, y, width, height, radius);
+
+        ctx.font = '19px "Press Start 2P"';
+        ctx.fillStyle = selectedIndex === index ? '#000' : 'white';
+        ctx.fillText(text, x + 5, y + 28); 
+    });
+}
+
+function moveMarker(e) {
+    if (e.key === 'ArrowUp' && selectedIndex > 0) {
+        selectedIndex--;
+    } else if (e.key === 'ArrowDown' && selectedIndex < games.length - 1) {
+        selectedIndex++;
+    } else if (e.key === 'Enter') {
+        window.location.href = gameUrls[selectedIndex];
+    }
+
+    draw();
+}
+
+draw();
+window.addEventListener('keydown', moveMarker);
